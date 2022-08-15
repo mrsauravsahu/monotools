62dd387161939403d4acc9b1ef3fcb269925a407
Saurav Sahu <mrsauravsahu@outlook.com>
2022-08-15 12:53:26 +0000

    improv: add checkout mode for semver-yeasy

--- apps/semver-yeasy/semver-yeasy.sh
+++ apps/semver-yeasy/semver-yeasy.sh
@@ -1,1 +1,23 @@
-# initial script
+#!/usr/bin/env bash
+
+echo 'Monotools: semver-yeasy'
+
+mode=$1
+
+case "${mode}" in
+
+checkout)
+    if [ "${GITHUB_EVENT_NAME}" = 'push' ]; then
+        DIFF_DEST="${GITHUB_REF_NAME}"
+    else
+        DIFF_DEST="${GITHUB_HEAD_REF}"
+    fi
+    git checkout ${DIFF_DEST}
+;;
+
+*)
+    echo 'Not a valid mode. Exiting...'
+    exit 0
+;;
+
+esac
