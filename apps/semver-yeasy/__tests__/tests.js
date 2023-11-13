55fe953f386c957d9ad1c0a100a5fc2d56535042
Example <mrsauravsahu@outlook.com>
2023-11-13 19:50:11 +0530

    test: add E2E tests

--- apps/semver-yeasy/__tests__/tests.js
+++ apps/semver-yeasy/__tests__/tests.js
@@ -3,52 +3,44 @@ import * as assert from 'uvu/assert';
 import { exec } from 'child-process-promise';
 import testConfig from './config.json' assert { type: "json" };
 
-var { ROOT_TEST_FOLDER } = process.env;
+var { ROOT_TEST_FOLDER, SEMVER_YEASY_ROOT_DIRECTORY } = process.env;
 await exec(`rm -rf ${ROOT_TEST_FOLDER}/test-workspaces || true`)
 
 testConfig.tests.forEach(currentTest => {
-  const currentTestWorkspace = `test-workspaces/tmp_${currentTest.name}`
-  const currentTestGitRepoPath = `test-workspaces/tmp_${currentTest.name}/repo`
-  const currentTestSourceCodePath = `test-workspaces/tmp_${currentTest.name}/repo/src`
+  const currentTestPath = currentTest.name.replace(': ', '__')
+  const currentTestWorkspace = `test-workspaces/${currentTestPath}`
+  const currentTestGitRepoPath = `test-workspaces/${currentTestPath}/repo`
+
   it.before(async () => {
     await exec(`
     mkdir -p ${ROOT_TEST_FOLDER}/${currentTestWorkspace}
     mkdir -p ${ROOT_TEST_FOLDER}/${currentTestGitRepoPath}
-    mkdir -p ${ROOT_TEST_FOLDER}/${currentTestSourceCodePath}
-    `)
-    await exec(`
-    cd ${ROOT_TEST_FOLDER}
-    cd ..
-    SEMVER_YEASY_ROOT_DIRECTORY=\`pwd\`
 
-    cd __tests__
     cd ${currentTestGitRepoPath}
-
     git config --global user.email 'example@example.com'
     git config --global user.name 'Example'
     git init
     `)
 
     for (var setupStep of currentTest.repoSetup) {
-      await exec(setupStep, { cwd: currentTestWorkspace });
+      await exec(setupStep, { cwd: currentTestGitRepoPath });
     }
   })
 
-  it(`${currentTest.name} - changed services`, async () => {
-    const data = await exec(`GITHUB_OUTPUT=\'${currentTestWorkspace}/output.changed-services.txt\' bash ../semver-yeasy.sh changed`)
+  it(`${currentTest.name}`, async () => {
+    const changesFileName = `../output.changes.txt`
 
-    assert.equal(data.stdout, `lul`)
+    await exec(
+      `GITHUB_OUTPUT=\'${changesFileName}\' bash ${SEMVER_YEASY_ROOT_DIRECTORY}/semver-yeasy.sh changed ${currentTest.inputs.env.GITVERSION_REPO_TYPE}`, {
+      env: currentTest.inputs.env,
+      cwd: currentTestGitRepoPath
+    })
+
+    const changesCmd = await exec(`cat ${changesFileName}`, { cwd: currentTestGitRepoPath })
+    assert.equal(changesCmd.stdout, currentTest.expectedOutputs.changes)
   })
 
-  it(`${currentTest.name} - PR Description`, async () => {
-    const data = await exec(`GITHUB_OUTPUT=\'${currentTestWorkspace}/output.pr-description.txt\' bash ../semver-yeasy.sh calculate-version`)
-
-    assert.equal(data.stdout, `Monotools: semver-yeasy
-"## impact surface\\\\nNo services changed\\\\n"
-`)
-  })
-
-  // it.after(async () => await exec(`rm -r ${currentTestWorkspace}`))
+  it.after(async () => await exec(`rm -r ${currentTestWorkspace}`))
 })
 
 it.run();
