f7d1764a855ffa6743821bb2a0dae912788dddd6
Example <mrsauravsahu@outlook.com>
2024-03-31 19:23:32 +0530

    fix: override svc prefix when calculating service version

--- apps/semver-yeasy/semver-yeasy.sh
+++ apps/semver-yeasy/semver-yeasy.sh
@@ -109,7 +109,9 @@ calculate-version)
         for svc in "${changed_services[@]}"; do
             CONFIG_FILE="${!CONFIG_FILE_VAR}"
             CONFIG_FILE=$(echo "${CONFIG_FILE}" | sed "s|\$svc|$svc|")
-            gitversion_calc_cmd="${GITVERSION_EXEC_PATH} $(pwd) /config ${CONFIG_FILE}"
+            svc_without_app_prefix=$(echo "${svc}" | sed "s|^app/||")
+            gitversion_calc_cmd="${GITVERSION_EXEC_PATH} $(pwd) /config ${CONFIG_FILE} /overrideconfig tag-prefix=${svc_without_app_prefix}" 
+            echo "Running calculation - '${gitversion_calc_cmd}'"
             gitversion_calc=$($gitversion_calc_cmd)
             
             # Used for debugging
