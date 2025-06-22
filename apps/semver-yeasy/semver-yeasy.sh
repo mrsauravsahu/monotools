44c946a9a7a61c170e9c307944afa1b1bb05a61e
mrsauravsahu <mrsauravsahu@outlook.com>
2025-06-23 02:18:00 +0530

    fix: service version calculation

--- apps/semver-yeasy/semver-yeasy.sh
+++ apps/semver-yeasy/semver-yeasy.sh
@@ -109,7 +109,7 @@ calculate-version)
     if [ "${repo_type}" = 'SINGLE_APP' ]; then
         service_versions_txt='## version bump\n'
         if [ "${SEMVERYEASY_CHANGED}" = 'true' ]; then
-        CONFIG_FILE="GITVERSION_CONFIG_${repo_type}"
+        CONFIG_FILE="${!CONFIG_FILE_VAR}"
         ${GITVERSION_EXEC_PATH} $(pwd) /config "${CONFIG_FILE}"
         gitversion_calc=$(${GITVERSION_EXEC_PATH} $(pwd) /config "${CONFIG_FILE}")
 
@@ -137,10 +137,9 @@ calculate-version)
         else
         service_versions_txt="## impact surface\n"
         for svc in "${changed_services[@]}"; do
-            CONFIG_FILE="${!CONFIG_FILE_VAR}"
             CONFIG_FILE=$(echo "${CONFIG_FILE}" | sed "s|\$svc|$svc|")
             svc_without_apps_prefix=$(echo "${svc}/" | sed "s|^apps/||")
-            gitversion_calc_cmd="${GITVERSION_EXEC_PATH} $(pwd) /config ${CONFIG_FILE} /overrideconfig tag-prefix=${svc_without_apps_prefix}" 
+            gitversion_calc_cmd="${GITVERSION_EXEC_PATH} $(pwd) /config ${CONFIG_FILE} /overrideconfig tag-prefix=${svc_without_apps_prefix}"
             log "Running calculation - '${gitversion_calc_cmd}'"
             gitversion_calc=$($gitversion_calc_cmd)
 
