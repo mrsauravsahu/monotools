c8a8354c2031805252eab3e71c6a6c7b1f7bd475
mrsauravsahu <mrsauravsahu@outlook.com>
2025-06-23 17:57:27 +0530

    fix: use /branch option to pass the correct branch name when calculating version

--- apps/semver-yeasy/semver-yeasy.sh
+++ apps/semver-yeasy/semver-yeasy.sh
@@ -106,12 +106,18 @@ changed)
 
 calculate-version)
     CONFIG_FILE_VAR="GITVERSION_CONFIG_${repo_type}"
+    if [ "GITHUB_EVENT_NAME" == 'pull_request' ]; then
+        GITVERSION_OPTIONS="/branch ${DIFF_DEST}"
+    else
+        GITVERSION_OPTIONS=''
+    fi
+
     if [ "${repo_type}" = 'SINGLE_APP' ]; then
         service_versions_txt='## version bump\n'
         if [ "${SEMVERYEASY_CHANGED}" = 'true' ]; then
         CONFIG_FILE="${!CONFIG_FILE_VAR}"
-        ${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config "${CONFIG_FILE}"
-        gitversion_calc=$(${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config "${CONFIG_FILE}")
+        # ${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config "${CONFIG_FILE} ${GITVERSION_OPTIONS}"
+        gitversion_calc=$(${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config "${CONFIG_FILE}" "${GITVERSION_OPTIONS}")
 
             GITVERSION_TAG_PROPERTY_NAME="GITVERSION_TAG_PROPERTY_$(echo "${DIFF_SOURCE}" | sed 's|/.*$||' | tr '[[:lower:]]' '[[:upper:]]')"
             GITVERSION_TAG_PROPERTY=${!GITVERSION_TAG_PROPERTY_NAME}
@@ -140,7 +146,7 @@ calculate-version)
             CONFIG_FILE="${!CONFIG_FILE_VAR}"
             CONFIG_FILE=$(echo "${CONFIG_FILE}" | sed "s|\$svc|$svc|")
             svc_without_apps_prefix=$(echo "${svc}/" | sed "s|^apps/||")
-            gitversion_calc_cmd="${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config ${CONFIG_FILE} /overrideconfig tag-prefix=${svc_without_apps_prefix}"
+            gitversion_calc_cmd="${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config ${CONFIG_FILE} /overrideconfig tag-prefix=${svc_without_apps_prefix} ${GITVERSION_OPTIONS}"
             log "Running calculation - '${gitversion_calc_cmd}'"
             gitversion_calc=$($gitversion_calc_cmd)
 
@@ -206,7 +212,7 @@ tag)
     git config --global user.name 'github-actions'
     if [ "${repo_type}" = 'SINGLE_APP' ]; then
         if [ "${SEMVERYEASY_CHANGED}" = 'true' ]; then
-        ${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config "${CONFIG_FILE}"
+        # ${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config "${CONFIG_FILE}"
         gitversion_calc=$(${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config "${CONFIG_FILE}")
 
         GITVERSION_TAG_PROPERTY_NAME="GITVERSION_TAG_PROPERTY_$(echo "${DIFF_SOURCE}" | sed 's|/.*$||' | tr '[[:lower:]]' '[[:upper:]]')"
@@ -233,7 +239,7 @@ tag)
         for svc in "${changed_services[@]}"; do
         echo "calculation for ${svc}"
         CONFIG_FILE=${!CONFIG_FILE_VAR//\$svc/$svc}
-        ${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config "${svc}/.gitversion.yml"
+        # ${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config "${svc}/.gitversion.yml"
         gitversion_calc=$(${GITVERSION_EXEC_PATH} $(pwd) /nonormalize /config "${svc}/.gitversion.yml")
 
         GITVERSION_TAG_PROPERTY_NAME="GITVERSION_TAG_PROPERTY_$(echo "${DIFF_SOURCE}" | sed 's|/.*$||' | tr '[[:lower:]]' '[[:upper:]]')"
